1b24a63455a3687c1aa2853df3e83cf859845960
bastiano86 <sebastiano_lamonaca@yahoo.com>
2021-09-14 15:53:53 +0200

    esercizio completato

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var app = new Vue({
     el: '#titolo',
-    inputUtente: {
-      richiesta1: prompt('scrivi un messaggio a tuo piacere'),
-      richiesta2: prompt('linka un\'immagine'),
-    }
+    data: {
+      richiesta1: 'Ciao',
+      richiestaImmagine:'https://www.gelestatic.it/thimg/BFgVJh3VRzyYdT2ZBRwuz5SwvS0=/fit-in/960x540/https%3A//www.lastampa.it/image/contentid/policy%3A1.40042259%3A1616074774/cat-4348355_960_720.jpg%3Ff%3Ddetail_558%26h%3D720%26w%3D1280%26%24p%24f%24h%24w%3Dafbdd83',
+    } 
+    
   })
