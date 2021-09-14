6a4ea1976dd52dd12c2904f22d055138e27fe618
bastiano86 <sebastiano_lamonaca@yahoo.com>
2021-09-14 15:34:32 +0200

    primo commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var app = new Vue({
+    el: '#titolo',
+    inputUtente: {
+      richiesta1: prompt('scrivi un messaggio a tuo piacere'),
+      richiesta2: prompt('linka un\'immagine'),
+    }
+  })
